19c050119ac79d3e5cf777d9aff6c494d863a005
Messi Li <caomeng.lcm@alibaba-inc.com>
2019-12-25 15:49:53 +0800

    feat: first version initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # bignumber-math-expression
 A JavaScript library for arbitrary-precision decimal and non-decimal arithmetic with math-expression
+
+## Features
+- Integers and decimals
+- NO API but full-featured `+ - * / ** %`
+
+## Usage
+`npm install bignumber-math-expression --save`
+
+ES6 module:
+
+```
+import BigNumber from 'bignumber-math-expression'
+
+BigNumber('0.1 + 0.2')					// "0.3"
+BigNumber('0.3 / (0.1 + 0.2)')			// "1"
+BigNumber('2 ** 53 + 1')				// "9007199254740993"
+```
+
+## License
+[MIT](https://github.com/licaomeng/bignumber-math-expression/blob/master/LICENSE)
---
+++ src/index.tsx
@@ -0,0 +1,92 @@
+import BN from 'bignumber.js'
+const bn = val => new BN(val)
+
+// Algorithm from http://csis.pace.edu/~wolf/CS122/infix-postfix.htm
+function infixToPostfix(exp) {
+  const infix = exp.match(/(\*\*)|[\+\-\*\/\%\(\)]|([0-9]+([\.][0-9]+)?)/g);
+  const presedences = ['-', '+', '**', '*', '/', '%'];
+
+  let opsStack: any[] = [];
+  let postfix: any[] = [];
+
+  for (let token of infix) {
+    // Step 1
+    if (typeof Number(token) === 'number' && isFinite(token)) {
+      postfix.push(token); continue;
+    }
+    let topOfStack = opsStack[opsStack.length - 1];
+    // Step 2
+    if (!opsStack.length || topOfStack == '(') {
+      opsStack.push(token); continue;
+    }
+    // Step 3
+    if (token == '(') {
+      opsStack.push(token); continue;
+    }
+    // Step 4
+    if (token == ')') {
+      while (opsStack.length) {
+        let op = opsStack.pop();
+        if (op == '(') break;
+        postfix.push(op);
+      }
+      continue;
+    }
+    // Step 5
+    let prevPresedence = presedences.indexOf(topOfStack);
+    let currPresedence = presedences.indexOf(token);
+    while (currPresedence < prevPresedence) {
+      let op = opsStack.pop();
+      postfix.push(op);
+      prevPresedence = presedences.indexOf(opsStack[opsStack.length - 1]);
+    }
+    opsStack.push(token);
+  }
+  // Step 6
+  while (opsStack.length) {
+    let op = opsStack.pop();
+    if (op == '(') break;
+    postfix.push(op);
+  }
+
+  return postfix;
+}
+
+const operatorMap = {
+  '+': 'plus',
+  '-': 'minus',
+  '*': 'times',
+  '/': 'div',
+  '**': 'pow',
+  '%': 'mod'
+}
+
+function evaluatePostfixExp(postfix) {
+  for (let [index, token] of postfix.entries()) {
+    if (/\+|\-|\*\*|\*|\/|\%/.test(token)) {
+      let grainExp = ''
+
+      if (/^[0-9]+([\.][0-9]+)?$/.test(postfix[index - 2])) {
+        grainExp = `bn(${postfix[index - 2]}).${operatorMap[token]}(${postfix[index - 1]})`
+      } else {
+        grainExp = `${postfix[index - 2]}.${operatorMap[token]}(${postfix[index - 1]})`
+      }
+
+      postfix[index] = grainExp
+      postfix.splice(index - 2, 2)
+
+      if (1 === postfix.length) {
+        break;
+      }
+      evaluatePostfixExp(postfix)
+    }
+  }
+  return `${postfix}.toString()`
+}
+
+export default (exp) => eval(evaluatePostfixExp(infixToPostfix(exp)))
+
+
+
+
+
